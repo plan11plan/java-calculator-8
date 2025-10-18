b7a0b7464080e9918d03b045628008e738faaec2
plan11plan <tjtfy7028@gmail.com>
2025-10-18 14:51:48 +0900

    feat: 구분자로 분리하여 숫자 추출 후 덧셈 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,15 +1,25 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
         String input = input();
+        int sum = splitByDefaultDelimiterAndSum(input);
+    }
 
+    private static int splitByDefaultDelimiterAndSum(String input) {
+        String[] split = input.split(",|:");
+        int sum = Arrays.stream(split)
+                .mapToInt(Integer::parseInt)
+                .sum();
+        return sum;
     }
 
     private static String input() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
+
 }
