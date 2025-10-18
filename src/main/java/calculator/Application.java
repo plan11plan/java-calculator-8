a226aaacdde55d4b2ac2bfe8153c4b3afc27f399
plan11plan <tjtfy7028@gmail.com>
2025-10-18 15:20:10 +0900

    feat: 음수 입력 예외처리 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,10 +4,21 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException {
         String input = input();
+        validateNegative(input);
         int sum = splitByDefaultDelimiterAndSum(input);
         output(sum);
+    }
+
+    private static void validateNegative(String input) throws IllegalArgumentException {
+        String[] split = input.split(",|:");
+        boolean hasNegative = Arrays.stream(split)
+                .mapToInt(Integer::parseInt)
+                .anyMatch(n -> n < 0);
+        if (hasNegative) {
+            throw new IllegalArgumentException("[ERROR] 음수는 입력할 수 없습니다.");
+        }
     }
 
     private static void output(Object sum) {
